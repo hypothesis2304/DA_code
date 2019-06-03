40120cb84605a9a9c93f60b82aad9b7015949982
Bhadrinath Nagabandi (Student) <bnagaban@asu.edu>
2019-06-03 13:39:03 -0700

    loss functions

---
+++ loss.py
@@ -0,0 +1,83 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import math
+import torch.nn.functional as F
+import pdb
+
+# def Entropy(input_):
+#     mask = input_.ge(0.000001)
+#     mask_out = torch.masked_select(input_, mask)
+#     entropy = -(torch.sum(mask_out * torch.log(mask_out)))
+#     return entropy / float(input_.size(0))
+
+class Entropy(nn.Module):
+    def __init__(self):
+        super(Entropy, self).__init__()
+
+    def forward(self, x):
+        b = F.softmax(x, dim=1) * F.log_softmax(x, dim=1)
+        b = -1.0 * b.sum()
+        return b
+
+def grl_hook(coeff):
+    def fun1(grad):
+        return -coeff*grad.clone()
+    return fun1
+
+def CDAN(input_list, ad_net, entropy=None, coeff=None, random_layer=None):
+    softmax_output = input_list[1].detach()
+    feature = input_list[0]
+    if random_layer is None:
+        op_out = torch.bmm(softmax_output.unsqueeze(2), feature.unsqueeze(1))
+        ad_out = ad_net(op_out.view(-1, softmax_output.size(1) * feature.size(1)))
+    else:
+        random_out = random_layer.forward([feature, softmax_output])
+        ad_out = ad_net(random_out.view(-1, random_out.size(1)))
+    batch_size = softmax_output.size(0) // 2
+    dc_target = torch.from_numpy(np.array([[1]] * batch_size + [[0]] * batch_size)).float().cuda()
+    if entropy is not None:
+        entropy.register_hook(grl_hook(coeff))
+        entropy = 1.0+torch.exp(-entropy)
+        source_mask = torch.ones_like(entropy)
+        source_mask[feature.size(0)//2:] = 0
+        source_weight = entropy*source_mask
+        target_mask = torch.ones_like(entropy)
+        target_mask[0:feature.size(0)//2] = 0
+        target_weight = entropy*target_mask
+        weight = source_weight / torch.sum(source_weight).detach().item() + \
+                 target_weight / torch.sum(target_weight).detach().item()
+        return torch.sum(weight.view(-1, 1) * nn.BCELoss(reduction='none')(ad_out, dc_target)) / torch.sum(weight).detach().item()
+    else:
+        return nn.BCELoss()(ad_out, dc_target)
+
+def DANN(features, ad_net):
+    ad_out = ad_net(features)
+    batch_size = ad_out.size(0) // 2
+    dc_target = torch.from_numpy(np.array([[1]] * batch_size + [[0]] * batch_size)).float().cuda()
+    return nn.BCELoss()(ad_out, dc_target)
+
+def update_ema_variables(model, ema_model, alpha, global_step):
+    alpha = min(1 - 1 / (global_step + 1), alpha)
+    for ema_param, param in zip(ema_model.parameters(), model.parameters()):
+        ema_param.data.mul_(alpha).add_(1 - alpha, param.data)
+
+def decision_boundary_transfer(source, target, margin):
+        loss = ((source + margin) ** 2 * ((source > -margin) & (target <= 0)).float() +
+                (source - margin) ** 2 * ((source <= margin) & (target > 0)).float())
+        return torch.abs(loss).sum()
+
+
+def softmax_mse_loss(input_logits, target_logits):
+    assert input_logits.size() == target_logits.size()
+    input_softmax = F.softmax(input_logits, dim=1)
+    target_softmax = F.softmax(target_logits, dim=1)
+    num_classes = input_logits.size()[1]
+    return F.mse_loss(input_softmax, target_softmax, size_average=False) / num_classes
+
+def softmax_kl_loss(input_logits, target_logits):
+    assert input_logits.size() == target_logits.size()
+    input_log_softmax = F.log_softmax(input_logits, dim=1)
+    target_softmax = F.softmax(target_logits, dim=1)
+    return F.kl_div(input_log_softmax, target_softmax, size_average=False)
